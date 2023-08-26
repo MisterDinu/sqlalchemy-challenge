31c89a8a4ec0d2227cae480e28d09463ccf58312
MisterDinu <134553073+MisterDinu@users.noreply.github.com>
2023-08-26 14:45:55 -0600

    final codes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # sqlalchemy-challenge
+
+Hello! I actually struggled a lot with this challenge, and had a lot of activities in my life, so I wasnt able
+to ask for any help, but I managed to get this challenge done, I hope it still get a grade for this.
+Just a little warning, when running de app.py do not use F5 to run it, I dont know why it doesnt work and says
+operational error, but when clicking on the play button from top right in VSCode, it works with no problems.
+
+Here I put all the references I used.
+
+https://stackoverflow.com/questions/70583312/why-am-i-getting-attributeerror-instancestate-has-no-post-inspect-on-simple-geoa
+
+https://www.tutorialspoint.com/python_pandas/python_pandas_descriptive_statistics.htm#:~:text=The%20describe()%20function%20computes,pertaining%20to%20the%20DataFrame%20columns.&text=This%20function%20gives%20the%20mean,given%20summary%20about%20numeric%20columns.
+
+https://stackoverflow.com/questions/993358/creating-a-range-of-dates-in-python
+
+https://matplotlib.org/stable/api/ticker_api.html
+
+https://matplotlib.org/stable/tutorials/introductory/pyplot.html
+
+https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.sort_values.html
+
+https://towardsdatascience.com/how-to-add-new-line-in-python-f-strings-7b4ccc605f4a
+
+https://stackoverflow.com/questions/4582264/python-sqlalchemy-order-by-datetime
+
+https://docs.sqlalchemy.org/en/20/orm/extensions/automap.html
+
+https://docs.sqlalchemy.org/en/14/orm/tutorial.html
+
+https://docs.sqlalchemy.org/en/14/orm/query.html#sqlalchemy.orm.Query.count
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,6 +1,7 @@
 # Import the dependencies.
-import os
 from flask import Flask
+import datetime as dt
+from datetime import datetime, timedelta
 import numpy as np
 import sqlalchemy as db
 from sqlalchemy.ext.automap import automap_base
@@ -9,18 +10,15 @@ from sqlalchemy.orm import query
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, text, inspect, MetaData, desc, func
 from pathlib import Path
-import json
+from flask import jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
-# database_path = Path("Resources", "hawaii.sqlite")
-detabase_path="C:/Users/Usuario/Butucamopo/Gits Butucamopo/MisterHacker Archivos PC/Challenges en Github/sqlalchemy-challenge/SurfsUp/Resources/hawaii.sqlite"
-
-# database_path = os.path.join('Resources', 'hawaii.sqlite')
-engine = create_engine(f"sqlite:///Resources/hawaii.sqlite")
+database_path = Path("Resources", "hawaii.sqlite")
+engine = create_engine(f"sqlite:///{database_path}")
 print(engine)
 
 # reflect an existing database into a new model
@@ -34,6 +32,8 @@ metadata = MetaData()
 station = Base.classes.station
 measurement = Base.classes.measurement
 
+most_repeated_id = "USC00519281"
+
 # Create our session (link) from Python to the DB
 Session = sessionmaker(bind=engine)
 
@@ -44,35 +44,117 @@ Session = sessionmaker(bind=engine)
 app= Flask(__name__)
 
 
-
-
 #################################################
 # Flask Routes
 #################################################
 @app.route("/")
 def welcome():
-    return ( 
+    return (
         "Welcome to homepage, these are the available routes:<br/>"
         f'/api/v1.0/precipitation<br/>'
         f'/api/v1.0/stations<br/>'
         f'/api/v1.0/tobs<br/>'
-        f'/api/v1.0/<start><br/>'
-        f'/api/v1.0/<start>/<end>'
-    
+        f'/api/v1.0/<start> (specify the start date after v1.0/, in format: %Y-%m-%d)<br/>'
+        f'/api/v1.0/<start>/<end> (after v1.0/, specify the start date, then, after the other "/", specify the end date, in format: %Y-%m-%d)'
     )
     
+# Just for last year
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session()
-    prcp_results = session.query(measurement.prcp)
+    latest_date = session.query(func.max(measurement.date)).scalar() 
+    latest_date = datetime.strptime(latest_date, '%Y-%m-%d')
+    start_date = latest_date - timedelta(days=365)
+    prcp_results = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date >= start_date)
     session.close()
-    all_prcp = list(np.ravel(prcp_results))
-    return jsonify(all_prcp)
+    prcp_list = []
+    for result in prcp_results:
+        prcp_dict ={
+            result[0]:result[1]
+        }
+        prcp_list.append(prcp_dict)
+    return jsonify(prcp_list)
 
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session()
+    stations = session.query(station).all()
+    stations_list = []
+    for st in stations:
+        station_dict = {
+            "Station ID":st.station,
+            "Station name":st.name
+        }
+        stations_list.append(station_dict)
+    return jsonify(stations_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session()
+    latest_date = session.query(func.max(measurement.date)).scalar() 
+    latest_date = datetime.strptime(latest_date, '%Y-%m-%d')
+    start_date = latest_date - timedelta(days=365)
+    most_repeated_tobs = session.query(measurement.date, measurement.tobs).\
+        filter(measurement.date >= start_date).\
+        filter(measurement.station == most_repeated_id)
+    session.close()
+    tobs_list = []
+    for tob in most_repeated_tobs:
+        tobs_dict ={
+            "Date":tob[0],
+            "Temperature(F)":tob[1]
+        }
+        tobs_list.append(tobs_dict)
+    return jsonify(tobs_list)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session()
+    retrieved_date_temps = session.query(measurement.tobs).\
+        filter(measurement.date >= start)
+    session.close()
+    date_tobs_list = []
+    for tob in retrieved_date_temps:
+        date_tobs_list.append(tob[0])
+    max_temp = max(date_tobs_list)
+    min_temp = min(date_tobs_list)
+    avg_temp = sum(date_tobs_list)/len(date_tobs_list)
+    # return f'Retrieved date: {date_tobs_list}'
+    return f'''
+    Retrieved date: {start}<br/>
+    Max temperature: {max_temp}<br/>
+    Min temperature: {min_temp}<br/>
+    Average temperature: {round(avg_temp, 3)}
+    '''
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end): 
+    session = Session()
+    retrieved_range_temps = session.query(measurement.tobs).\
+        filter(measurement.date >= start).\
+        filter(measurement.date <= end)
+    session.close()
+    range_tobs_list = []
+    for tob in retrieved_range_temps:
+        range_tobs_list.append(tob[0])
+    max_temp = max(range_tobs_list)
+    min_temp = min(range_tobs_list)
+    avg_temp = sum(range_tobs_list)/len(range_tobs_list)
+    # return f'Retrieved date: {range_tobs_list}'
+    return f'''
+    Retrieved date: {start}<br/>
+    Max temperature: {max_temp}<br/>
+    Min temperature: {min_temp}<br/>
+    Average temperature: {round(avg_temp, 3)}
+    '''
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
+
 
 
